c4b078d9cffe9af011cf7abba05e4f786898a692
ardakt1m <ardaktym.meirmanov@astana.fizmat.kz>
2022-10-16 17:26:17 +0600

    updated

--- src/main/java/lang/print/gaps/task1/BrokenApp.java
+++ src/main/java/lang/print/gaps/task1/BrokenApp.java
@@ -1,7 +1,9 @@
 package lang.print.gaps.task1;
 
+import java.util.logging.Logger;
+
 public class BrokenApp {
     public static void main(String[] args) {
-        System.out.println("fixMe");
+        Logger.getLogger("fixMe");
     }
 }
